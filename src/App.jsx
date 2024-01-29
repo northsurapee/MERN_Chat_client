c786ac6d8520a26486d08c6249c88e518621373f
Surapee <suwan.srp@gmail.com>
2024-01-30 00:08:20 +0700

    Config .env

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,11 @@ import { UserContextProvider } from "./UserContext";
 import Routes from "./Routes";
 
 function App() {
-  axios.defaults.baseURL = "http://localhost:4040";
+  if (import.meta.env.VITE_NODE_ENV === "development") {
+    axios.defaults.baseURL = import.meta.env.VITE_DEV_API_BASE_URL;
+  } else {
+    axios.defaults.baseURL = import.meta.env.VITE_PRO_API_BASE_URL;
+  }
   axios.defaults.withCredentials = true;
 
   return (
